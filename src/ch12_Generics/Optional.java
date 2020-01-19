fab09a3bf23b78bd1e0d28169979e223be173ed2
younah shin <roaving@gmail.com>
2020-01-19 13:01:58 +0900

    casting Generics

---
+++ src/ch12_Generics/Optional.java
@@ -0,0 +1,17 @@
+package ch12_Generics;
+
+public final class Optional<T> {
+	private static final Optional<?> EMPTY = new Optional<>();
+//	private final T value;
+	
+	public static<T> Optional<T> empty() {
+		Optional<T> t = (Optional<T>) EMPTY;
+		return t;
+	}
+	
+	Optional<?> wopt = new Optional<Object>();
+	Optional<Object> oopt = new Optional();
+	
+	Optional<String> sopt = (Optional<String>)wopt;	//형변환 가능
+//	Optional<String> sopt = (Optional<String>)oopt; //형변환 불가능 error
+}
