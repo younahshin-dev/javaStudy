9404506938e5366e9687d613d1de5da4d662d5aa
younah shin <roaving@gmail.com>
2020-01-21 21:37:54 +0900

    ThreadEx14

---
+++ src/ch12_Generics_enums_annotation/ThreadEx14.java
@@ -0,0 +1,17 @@
+package ch12_Generics_enums_annotation;
+
+import javax.swing.JOptionPane;
+
+public class ThreadEx14 {
+	public static void main(String[] args) {
+		ThreadEx14_1 th1 = new ThreadEx14_1();
+		th1.start();
+		
+		String input = JOptionPane.showInputDialog("아무값이나 입력하세요.");
+		System.out.println("입력하신 값은 "+input+" 입니다.");
+		th1.interrupt();
+		System.out.println("isInterrupted() :"+ th1.isInterrupted());
+	}
+	
+	
+}
---
+++ src/ch12_Generics_enums_annotation/ThreadEx14_1.java
@@ -0,0 +1,20 @@
+package ch12_Generics_enums_annotation;
+
+public class ThreadEx14_1 extends Thread {
+
+	public void run() {
+		int i = 10;
+		
+		while (i!=0 && !isInterrupted()) {
+			System.out.println(i--);
+			try {
+				Thread.sleep(1000); //1초지연
+			} catch (InterruptedException e) {
+				interrupt();
+			}
+		}
+		
+		System.out.println("카운트가 종료되었습니다.");
+	}
+
+}
